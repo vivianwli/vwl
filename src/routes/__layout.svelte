ee6ff1303eb9b99ea3cee1166d4b42f24dde3821
teacupkittie <vivieli77@gmail.com>
2021-12-24 21:17:46 -0500

    header and humble beginnings

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,48 @@
+<script>
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <meta name="robots" content="noindex" />
+</svelte:head>
+
+<div class="header-container">
+    <div class="header">
+        <nav>
+            <a href=".">home</a>
+            <a href="/portfolio">portfolio</a>
+            <a href="/blog">blog</a>
+        </nav>
+        <div class="dark-switch">
+            <ion-icon name="moon-outline"></ion-icon>
+        </div>
+    </div>
+</div>
+
+<slot />
+
+<style>
+    .header-container {
+        position: fixed;
+    }
+    .header {
+        display: grid;
+        grid-template-columns: min-content 1fr min-content;
+    }
+    .header .dark-switch {
+        grid-column-start: 3;
+        color: var(--secondary-color);
+        font-size: 2rem;
+        padding: 1rem;
+    }
+    nav {
+        padding: 1rem;
+    }
+    a {
+        display: block;
+        text-decoration: none;
+        color: var(--secondary-color);
+        margin: 1rem;
+        font-size: 1.2rem;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // import IoArrowForwardCircleOutline from "svelte-icons-pack/io/IoArrowForwardCircleOutline";
+    
+    let src = 'src/images/vwl.svg'
+</script>
+
+<div class="logo">
+    <img {src} alt="logo" />
+</div>
+
+<!-- <h1>hello! i'm vivian li.</h1>
+
+<p>byeeeeeeeeeeee</p> -->
+
+<style>
+    .logo {
+        width: 40vw;
+        margin: auto;
+        padding-top: 3rem;
+    }
+    .logo img {
+        width: 100%;
+        margin: auto;
+    }
+</style>
